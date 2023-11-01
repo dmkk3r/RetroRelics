b34720606a0f65047e0978aa3b7295480af91b9e
dmkk3r <dominik.koerber.95@gmail.com>
2023-11-01 21:14:51 +0100

    Sveltekit and tailwind project with basic appshell

---
+++ src/web/src/lib/components/shell/Navbar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Book, Home, LibraryBig, LogOut, Settings } from 'lucide-svelte';
+</script>
+
+<div class="flex h-28 flex-col bg-stone-900 font-mono">
+	<div class="flex">
+		<a
+			href="/"
+			class="flex items-start justify-center rounded-br-2xl bg-stone-800 hover:bg-stone-700"
+		>
+			<h1 class="px-6 py-3 text-lg font-bold text-white">RetroRelics</h1>
+		</a>
+		<div class="flex flex-grow items-center justify-end pr-6">
+			<button class="rounded-full p-2 hover:bg-stone-700">
+				<LogOut color="white" size="20" />
+			</button>
+		</div>
+	</div>
+	<div class="flex flex-grow flex-row items-center space-x-12 pl-6">
+		<span class="flex flex-row items-center space-x-2">
+			<Home color="#ffffff" size="18" />
+			<p class="text-sm text-white">Home</p>
+		</span>
+		<span class="flex flex-row items-center space-x-2">
+			<LibraryBig color="#ffffff" size="18" />
+			<p class="text-sm text-white">Collections</p>
+		</span>
+		<span class="flex flex-row items-center space-x-2">
+			<Book color="#ffffff" size="18" />
+			<p class="text-sm text-white">Relics</p>
+		</span>
+		<span class="flex flex-row items-center space-x-2">
+			<Settings color="#ffffff" size="18" />
+			<p class="text-sm text-white">Settings</p>
+		</span>
+	</div>
+</div>
---
+++ src/web/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Footer from '$lib/components/shell/Footer.svelte';
+	import Navbar from '$lib/components/shell/Navbar.svelte';
+	import Sidebar from '$lib/components/shell/Sidebar.svelte';
+	import '../app.css';
+</script>
+
+<div class="w-screen h-screen flex flex-col bg-stone-950">
+	<Navbar />
+	<div class="flex flex-row h-full">
+		<div class="flex h-full">
+
+			<Sidebar />
+		</div>
+		<div class="flex flex-grow">
+			<slot />
+		</div>
+	</div>
+	<Footer />
+</div>
